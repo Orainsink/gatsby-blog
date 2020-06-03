504ed4108633ebc2d894cf204a0d42afbc0e1491
Orainsink <ywt1250066597@gmail.com>
2020-06-03 21:07:36 +0800

    Update

---
+++ src/components/Dynamic.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Canvas, useFrame } from "react-three-fiber"
+
+interface Props {
+}
+const Dynamic :React.FC<Props> =() => {
+
+  return (<>
+    
+  </>
+  );
+};
+
+export default React.memo(Dynamic);
