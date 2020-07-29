fb65733771de0537e4b3b728293e030d85ef60fb
Orainsink <ywt1250066597@gmail.com>
2020-07-29 21:04:14 +0800

    Updarte: ::footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,50 +1,63 @@
 import React from 'react';
 import { GithubOutlined, MailOutlined } from '@ant-design/icons';
 import styles from '../styles/Footer.module.less';
+import { ReactComponent as ValleySvg } from '../assets/img/valley.svg';
 
 /**Footer */
 const Footer: React.FC = () => {
   return (
-    <footer className={styles.wrapper}>
-      <div className={styles.phrase}>
+    <footer className={styles.wrap}>
+      <div className={styles.valley}></div>
+      <div className={styles.main}>
+        {/* <ValleySvg className={styles.valley} /> */}
+        <div className={styles.phrase}>
+          <div>
+            <GithubOutlined
+              className={styles.icon}
+              style={{ transform: 'translateY(-1px)' }}
+            />
+            <a
+              href="https://github.com/Orainsink/gatsby-blog"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Site Repository
+            </a>
+          </div>
+          <div>
+            <MailOutlined
+              className={styles.icon}
+              style={{ transform: 'translateY(1px)' }}
+            />
+            Email: ywt1250066597@gmail.com
+          </div>
+        </div>
         <div>
-          <GithubOutlined
-            className={styles.icon}
-            style={{ transform: 'translateY(-1px)' }}
-          />
+          © Orainsink {new Date().getFullYear()}, Built with{' '}
           <a
-            href="https://github.com/Orainsink/gatsby-blog"
+            href="https://www.gatsbyjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Site Repository
+            Gatsby
+          </a>
+          {', '}
+          <a
+            href="https://ant.design"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Antd
+          </a>
+          {', '}
+          <a
+            href="https://threejs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Three.js
           </a>
         </div>
-        <div>
-          <MailOutlined
-            className={styles.icon}
-            style={{ transform: 'translateY(1px)' }}
-          />
-          Email: ywt1250066597@gmail.com
-        </div>
-      </div>
-      <div>
-        © Orainsink {new Date().getFullYear()}, Built with{' '}
-        <a
-          href="https://www.gatsbyjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gatsby
-        </a>
-        {', '}
-        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-          Antd
-        </a>
-        {', '}
-        <a href="https://threejs.org" target="_blank" rel="noopener noreferrer">
-          Three.js
-        </a>
       </div>
     </footer>
   );
