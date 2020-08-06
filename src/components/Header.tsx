728c8953c1cc00fb5087e1f3e0742477e2632280
Orainsink <ywt1250066597@gmail.com>
2020-08-06 15:38:44 +0800

    perf: ⚡️ split menuDrawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,65 +1,16 @@
 import React, { useEffect, useCallback, useState, useMemo } from 'react';
 import styles from '../styles/Header.module.less';
 import classnames from 'classnames';
-import { Row, Col, Drawer, Button } from 'antd';
+import { Row, Col } from 'antd';
 import { useStaticQuery, graphql, navigate } from 'gatsby';
 import { Link } from 'gatsby';
-import { GithubOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import { GithubOutlined } from '@ant-design/icons';
 import { ReactComponent as ArrowSvg } from '../assets/img/arrow.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import useWindowSize from '../hooks/useWindowSize';
 import loadable from '@loadable/component';
 const MyPlayer = loadable(() => import('../components/MyPlayer'));
-
-/**抽屉菜单,用于移动端兼容 */
-const MenuDrawer: React.FC<{ location: any }> = ({ location }) => {
-  const [visible, setVisible] = useState(false);
-  const { scene } = useSelector((state) => state);
-
-  /**路径改变时关闭菜单 */
-  useEffect(() => {
-    setVisible(false);
-  }, [location]);
-
-  /**fix bug,当drawer关闭时,antd组件会重设body的style,导致滚动失效 */
-  useEffect(() => {
-    if (!visible) {
-      const body = document.getElementsByTagName('body')[0];
-      body.style.overflowY = scene ? 'hidden' : 'auto';
-    }
-  }, [visible, scene]);
-
-  return (
-    <>
-      <Button
-        size="middle"
-        ghost
-        className={styles.drawerBtn}
-        icon={<UnorderedListOutlined />}
-        onClick={() => setVisible(true)}
-      >
-        MENU
-      </Button>
-      <Drawer
-        title={<span className={styles.drawerTitle}>MENU</span>}
-        visible={visible}
-        onClose={() => setVisible(false)}
-      >
-        <ul className={classnames(styles.nav, styles.drawerNav)}>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/archives">archives</Link>
-          </li>
-          <li>
-            <Link to="/about">about</Link>
-          </li>
-        </ul>
-      </Drawer>
-    </>
-  );
-};
+const MenuDrawer = loadable(() => import('../components/MenuDrawer'));
 
 /**Header */
 const Header: React.FC<{ location: any }> = ({ location }) => {
