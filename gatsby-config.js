16a574298bccea5b564c51ff240264de83080f8c
Orainsink <ywt1250066597@gmail.com>
2020-08-13 21:01:49 +0800

    feat: 🎸 配置algolia, 将谷歌分析代码移至环境变量

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: `Orainsink's blog`,
@@ -75,7 +77,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-173159064-1`,
+        trackingId: process.env.TRACKING_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require('./src/utils/algolia-queries'),
       },
     },
     `gatsby-plugin-feed`,
