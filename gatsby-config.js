ffc0a68455ad473ac609ed6d0a31c2cb1a0bf225
Orainsink <ywt1250066597@gmail.com>
2020-08-17 00:28:27 +0800

    Test: :white_check_mark: (test key)

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
       resolve: `gatsby-plugin-algolia`,
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
         indexName: process.env.ALGOLIA_INDEX_NAME,
         queries: require('./src/utils/algolia-queries'),
       },
