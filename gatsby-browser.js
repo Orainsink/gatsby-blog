659adb92356a040ded64ee574bf0959c70dbb7b3
Orainsink <ywt1250066597@gmail.com>
2020-08-28 15:40:55 +0800

    Feat: :sparkles: (Spinning) Add app spinning

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,15 @@ import './src/assets/theme/prism-line-number-custom.css';
 
 import GlobalLayout from './src/layout/GlobalLayout';
 
+export const onInitialClientRender = () => {
+  setTimeout(function () {
+    document.getElementById('___loader').style.opacity = 0;
+  }, 600);
+  setTimeout(function () {
+    document.getElementById('___loader').style.display = 'none';
+  }, 1000);
+};
+
 export const wrapPageElement = ({ element, props }) => {
   return <GlobalLayout {...props}>{element}</GlobalLayout>;
 };
--- src/html.js
+++ src/html.js
@@ -15,8 +15,60 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
-        <Loading />
         {props.preBodyComponents}
+        <div
+          key="loader"
+          id="___loader"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background: '#0a0a0a',
+            transition: 'opacity 0.3s Linear',
+            opacity: 1,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            overflow: 'hidden',
+            zIndex: 9999,
+          }}
+        >
+          <svg
+            version="1.1"
+            id="loader-1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="260px"
+            height="260px"
+            viewBox="0 0 40 40"
+            xmlSpace="preserve"
+          >
+            <path
+              id=""
+              opacity="0"
+              fill="transparent"
+              d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z"
+            />
+            <path
+              id="orbit"
+              d="m20.201,15.91701c-9.20089,0 -16.6606,3.28421 -16.6606,7.335c0,4.05128 7.4597,7.335 16.6606,7.335s16.6606,-3.28372 16.6606,-7.335c-0.00111,-4.05079 -7.4597,-7.335 -16.6606,-7.335z"
+              strokeOpacity="0"
+              strokeWidth=".1"
+              stroke="transparent"
+              fill="transparent"
+            />
+            <circle id="mun" r="1" fill="#fff">
+              <animateMotion id="animation" dur="2s" repeatCount="indefinite">
+                <mpath xlinkHref="#orbit"></mpath>
+              </animateMotion>
+            </circle>
+            <circle cx="20" cy="20" r="5" fill="#fff"></circle>
+          </svg>
+        </div>
         <div
           key={`body`}
           id="___gatsby"
