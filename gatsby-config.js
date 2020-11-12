adbfdf41dd4d1fc89416fbc0e6eadd774ba8e5ae
Orainsink <ywt1250066597@gmail.com>
2020-11-12 13:48:04 +0800

    Prune: :fire:

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,6 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        path: `${__dirname}/content/assets`,
         name: `assets`,
       },
     },
