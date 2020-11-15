fec76ba6ae200cda7cf2eaa5b9c6446f355ccf34
Orainsink <ywt1250066597@gmail.com>
2020-11-15 17:57:40 +0800

    Ignore: :see_no_evil: change gitignore

---
+++ typings/global.d.ts
@@ -0,0 +1,24 @@
+declare const __PATH_PREFIX__: string;
+declare const __REDUX_DEVTOOLS_EXTENSION__: string;
+declare module '*.less';
+declare module '*.json';
+declare module '*.module.less' {
+  const classes: { [className: string]: string };
+  export default classes;
+}
+declare module '*.gltf';
+declare module '*.svg';
+declare module 'algoliasearch/lite';
+declare module 'react-redux';
+declare module '@loadable/component';
+declare module 'classnames';
+
+interface MdxItem extends MarkdownRemark {
+  body?: string;
+}
+
+interface ChildMdxItem {
+  node: {
+    childMdx: MdxItem;
+  };
+}
