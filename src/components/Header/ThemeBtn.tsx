b517dd5c6c5c2893b3c0897d56f52e8049bc380f
Orainsink <ywt1250066597@gmail.com>
2020-12-28 10:53:28 +0800

    WIP: :construction: (theme btn)

--- src/components/Header/ThemeBtn.tsx
+++ src/components/Header/ThemeBtn.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
-import { Switch } from 'antd';
 import { useDispatch } from 'react-redux';
+import classnames from 'classnames';
+import styles from '../../styles/ThemeBtn.module.less';
 
 const ThemeBtn = () => {
   const dispatch = useDispatch();
@@ -15,18 +16,22 @@ const ThemeBtn = () => {
   return (
     <ThemeToggler>
       {({ theme, toggleTheme }) => {
-        if (theme == null) {
-          return null;
-        }
+        if (!theme) return;
+
         setTheme(theme);
         return (
-          <Switch
-            defaultChecked
-            onChange={(checked: boolean) =>
-              toggleTheme(checked ? 'dark' : 'light')
-            }
-            checked={theme === 'dark'}
-          />
+          <div className={classnames(styles.toggle, styles.daynight)}>
+            <input
+              type="checkbox"
+              id="toggle--daynight"
+              className={styles.checkbox}
+              onChange={(e) => toggleTheme(e.target.checked ? 'light' : 'dark')}
+              checked={theme !== 'dark'}
+            />
+            <label className={styles.btn} htmlFor="toggle--daynight">
+              <span className={styles.feature}></span>
+            </label>
+          </div>
         );
       }}
     </ThemeToggler>
