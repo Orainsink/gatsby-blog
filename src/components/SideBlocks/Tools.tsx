f76c6536730bc883792d63c1ec9dc06af662ec56
Orainsink <ywt1250066597@gmail.com>
2021-01-15 17:25:28 +0800

    Dep: :package: (add react-spring)

--- src/components/SideBlocks/Tools.tsx
+++ src/components/SideBlocks/Tools.tsx
@@ -5,6 +5,53 @@ import styles from '../../styles/SideBar.module.less';
 import tools from '../../assets/js/tools';
 import Icon from '@ant-design/icons';
 import useColFlex from './useColFlex';
+import { useSpring, animated } from 'react-spring';
+
+const calc = (x: number, y: number) => [
+  -(y - window?.innerHeight / 2) / 20,
+  (x - window?.innerWidth / 2) / 20,
+  1.1,
+];
+const trans: any = (x: number, y: number, s: number) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+interface ToolItemProp {
+  data: {
+    name: string;
+    icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    url: string;
+  };
+}
+
+const ToolItem = React.memo(({ data }: ToolItemProp) => {
+  const [props, setProps] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
+  return (
+    <animated.a
+      href={data.url}
+      onMouseMove={({ clientX: x, clientY: y }) =>
+        setProps({ xys: calc(x, y) })
+      }
+      onMouseLeave={() => setProps({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans) }}
+      target="_blank"
+      rel="noreferrer"
+      title={data.url}
+      key={data.name}
+      className={styles.toolLink}
+    >
+      <span className={styles.tool}>
+        <div className={styles.iconBox}>
+          <Icon component={data.icon} className={styles.toolIcon}></Icon>
+        </div>
+        <span className={styles.toolName}>{data.name}</span>
+      </span>
+    </animated.a>
+  );
+});
 
 /* Tools url */
 const Tools = () => {
@@ -14,21 +61,7 @@ const Tools = () => {
     <Col flex={colFlex} className={classnames(styles.col, styles.toolsWrapper)}>
       <div className={styles.title}>Tools</div>
       {tools.map((tool) => (
-        <a
-          href={tool.url}
-          target="_blank"
-          rel="noreferrer"
-          title={tool.url}
-          key={tool.name}
-          className={styles.toolLink}
-        >
-          <span className={styles.tool}>
-            <div className={styles.iconBox}>
-              <Icon component={tool.icon} className={styles.toolIcon}></Icon>
-            </div>
-            <span className={styles.toolName}>{tool.name}</span>
-          </span>
-        </a>
+        <ToolItem data={tool} />
       ))}
     </Col>
   );
