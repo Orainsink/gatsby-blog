e33adb0cc25d1470cf972e5178aac85431756cae
Orainsink <ywt1250066597@gmail.com>
2021-07-19 11:47:34 +0800

    Fix: :bug:

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,7 @@ const onInitialClientRender = () => {
 
   if (
     !Boolean(globalThis?.localStorage.getItem('SCENE')) ||
-    Boolean(globalThis？.localStorage.getItem('SKIP'))
+    Boolean(globalThis?.localStorage.getItem('SKIP'))
   ) {
     document.getElementById('___loader').style.display = 'none';
   } else {
