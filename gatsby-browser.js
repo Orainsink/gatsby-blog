edc16347c851b47fba132bf28b399e8c3755fc11
Orainsink <ywt1250066597@gmail.com>
2021-07-19 09:45:06 +0800

    Pref: :zap: improve loading effects

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,9 +4,17 @@ const onInitialClientRender = () => {
   setTimeout(function () {
     document.getElementById('___loader').style.opacity = 0;
   }, 0);
-  setTimeout(function () {
+
+  if (
+    !Boolean(globalThis?.localStorage.getItem('SCENE')) ||
+    Boolean(globalThis.localStorage?.getItem('SKIP'))
+  ) {
     document.getElementById('___loader').style.display = 'none';
-  }, 300);
+  } else {
+    setTimeout(function () {
+      document.getElementById('___loader').style.display = 'none';
+    }, 300);
+  }
 };
 
 module.exports = {
