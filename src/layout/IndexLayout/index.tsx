3d2ebdab9c9e45e047a4ccb2b764bd2ac5a2b8ed
Orainsink <ywt1250066597@gmail.com>
2021-09-30 11:32:27 +0800

    Revert: :rewind: revert useSpring

--- src/layout/IndexLayout/index.tsx
+++ src/layout/IndexLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import { useSelector, useDispatch } from 'react-redux';
 import { Col, Row } from 'antd';
@@ -9,43 +9,16 @@ import Comment from '../../components/Comment';
 import { iRootState } from '../../redux/store';
 import Footer from '../../components/Footer';
 import Tools from '../../components/SideBlocks/Tools';
-import { useSpring, a } from 'react-spring';
 
 interface Props {
   children?: React.ReactNode;
 }
 
-const SCENE_STATUS = {
-  ACTIVE: 'ACTIVE',
-  DIS_ACTIVE: 'DISACTIVE',
-  TRIGGER: 'TRIGGER',
-};
-
-const getTop = (status: string) => {
-  const topObj = {
-    [SCENE_STATUS.ACTIVE]: '100vh',
-    [SCENE_STATUS.DIS_ACTIVE]: '0',
-    [SCENE_STATUS.TRIGGER]: '90vh',
-  };
-  return topObj[status];
-};
-
 /**index Layout */
 const Layout = ({ children }: Props) => {
   const { scene, trigger, skip } = useSelector((state: iRootState) => state);
   const dispatch = useDispatch();
-
-  const status = useMemo(() => {
-    if (!scene || skip) {
-      return SCENE_STATUS.DIS_ACTIVE;
-    }
-    if (scene && !skip && trigger) {
-      return SCENE_STATUS.TRIGGER;
-    }
-    if (scene && !skip && !trigger) {
-      return SCENE_STATUS.ACTIVE;
-    }
-  }, [scene, skip, trigger]);
+  const [wrapperClass, setWrapperClass] = useState('');
 
   useBackgroundColor(skip);
 
@@ -54,19 +27,22 @@ const Layout = ({ children }: Props) => {
     dispatch({ type: 'CUR_TAG', payload: '' });
   }, [dispatch]);
 
-  const spring = useSpring({
-    top: getTop(status),
-    config: { tension: 560, friction: 13 },
-  });
+  useEffect(() => {
+    setWrapperClass(
+      !scene || skip
+        ? styles.disActive
+        : trigger
+        ? styles.trigger
+        : styles.active
+    );
+  }, [scene, skip, trigger]);
 
   return (
-    <a.div
-      className={classnames(styles.wrapper)}
-      style={spring}
-      id="markdownBody"
-    >
-      {status !== SCENE_STATUS.DIS_ACTIVE && (
-        <div className={classnames(styles.clickTip, trigger && styles.show)}>
+    <div className={classnames(styles.wrapper, wrapperClass)} id="markdownBody">
+      {scene && !skip && (
+        <div
+          className={classnames(styles.clickTip, trigger ? styles.show : null)}
+        >
           Click to slide
         </div>
       )}
@@ -84,7 +60,7 @@ const Layout = ({ children }: Props) => {
         </Row>
       </main>
       <Footer />
-    </a.div>
+    </div>
   );
 };
 
