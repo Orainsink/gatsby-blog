cdba5e8ccabc51eb83455d78bc88610581673568
Orainsink <ywt1250066597@gmail.com>
2021-12-21 01:12:52 +0800

    Style: :art:

--- plugins/gatsby-plugin-loadable-components-ssr/gatsby-browser.js
+++ plugins/gatsby-plugin-loadable-components-ssr/gatsby-browser.js
@@ -2,25 +2,22 @@ const { render, hydrate } = require('react-dom');
 const { loadableReady } = require('@loadable/component');
 
 /**Allow a plugin to replace the ReactDOM.render/ReactDOM.hydrate function call by a custom renderer. */
-const replaceHydrateFunction = (_, options) => (
-  element,
-  container,
-  callback
-) => {
-  loadableReady(() => {
-    const renderFn =
-      typeof options.useHydrate === 'undefined'
-        ? // Using ReactDOM.hydrate on develop will throw an error in console
-          process.env.GATSBY_BUILD_STAGE.includes('develop')
-          ? render
-          : hydrate
-        : !!options.useHydrate
-        ? hydrate
-        : render;
+const replaceHydrateFunction =
+  (_, options) => (element, container, callback) => {
+    loadableReady(() => {
+      const renderFn =
+        typeof options.useHydrate === 'undefined'
+          ? // Using ReactDOM.hydrate on develop will throw an error in console
+            process.env.GATSBY_BUILD_STAGE.includes('develop')
+            ? render
+            : hydrate
+          : !!options.useHydrate
+          ? hydrate
+          : render;
 
-    renderFn(element, container, callback);
-  });
-};
+      renderFn(element, container, callback);
+    });
+  };
 
 module.exports = {
   replaceHydrateFunction,
