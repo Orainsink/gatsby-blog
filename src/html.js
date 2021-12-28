dcb93d61341567e5490d4ea6d8e67edaedc4a2b7
Orainsink <ywt1250066597@gmail.com>
2021-12-28 17:09:54 +0800

    Prune: :fire:

--- src/html.js
+++ src/html.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
  * svg animations always started after the page is fully loaded,
  * I tried many solutions, but only iframe work.
  */
-const loadingIframe = `<iframe width="261px" height="261px" style="border:0;margin:0;padding:0;overflow:hidden;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iMjYwIiBoZWlnaHQ9IjI2MCIgdmlld0JveD0iMCAwIDQwIDQwIj48cGF0aCBmaWxsPSJ0cmFuc3BhcmVudCIgc3Ryb2tlPSJ0cmFuc3BhcmVudCIgc3Ryb2tlLXdpZHRoPSIuMSIgc3Ryb2tlLW9wYWNpdHk9IjAiIGQ9Ik0uMjcxLS4yODhjLTkuMiAwLTE2LjY2IDMuMjg0LTE2LjY2IDcuMzM1czcuNDYgNy4zMzUgMTYuNjYgNy4zMzUgMTYuNjYxLTMuMjg0IDE2LjY2MS03LjMzNUMxNi45MzEgMi45OTcgOS40NzItLjI4OC4yNzItLjI4OHoiIGlkPSJhIi8+PGNpcmNsZSByPSIxIiBjeD0iMjAiIGN5PSIxNiIgZmlsbD0iI2ZmZiI+PGFuaW1hdGVNb3Rpb24gZHVyPSIycyIgcmVwZWF0Q291bnQ9ImluZGVmaW5pdGUiPjxtcGF0aCB4bGluazpocmVmPSIjYSIvPjwvYW5pbWF0ZU1vdGlvbj48L2NpcmNsZT48Y2lyY2xlIGN4PSIyMCIgY3k9IjIwIiByPSI1IiBmaWxsPSIjZmZmIi8+PC9zdmc+"></iframe>`;
-const showLoader =
-  Boolean(globalThis?.localStorage?.getItem('SCENE')) &&
-  !Boolean(globalThis?.localStorage?.getItem('SKIP'));
+// const loadingIframe = `<iframe width="261px" height="261px" style="border:0;margin:0;padding:0;overflow:hidden;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iMjYwIiBoZWlnaHQ9IjI2MCIgdmlld0JveD0iMCAwIDQwIDQwIj48cGF0aCBmaWxsPSJ0cmFuc3BhcmVudCIgc3Ryb2tlPSJ0cmFuc3BhcmVudCIgc3Ryb2tlLXdpZHRoPSIuMSIgc3Ryb2tlLW9wYWNpdHk9IjAiIGQ9Ik0uMjcxLS4yODhjLTkuMiAwLTE2LjY2IDMuMjg0LTE2LjY2IDcuMzM1czcuNDYgNy4zMzUgMTYuNjYgNy4zMzUgMTYuNjYxLTMuMjg0IDE2LjY2MS03LjMzNUMxNi45MzEgMi45OTcgOS40NzItLjI4OC4yNzItLjI4OHoiIGlkPSJhIi8+PGNpcmNsZSByPSIxIiBjeD0iMjAiIGN5PSIxNiIgZmlsbD0iI2ZmZiI+PGFuaW1hdGVNb3Rpb24gZHVyPSIycyIgcmVwZWF0Q291bnQ9ImluZGVmaW5pdGUiPjxtcGF0aCB4bGluazpocmVmPSIjYSIvPjwvYW5pbWF0ZU1vdGlvbj48L2NpcmNsZT48Y2lyY2xlIGN4PSIyMCIgY3k9IjIwIiByPSI1IiBmaWxsPSIjZmZmIi8+PC9zdmc+"></iframe>`;
+// const showLoader =
+//   Boolean(globalThis?.localStorage?.getItem('SCENE')) &&
+//   !Boolean(globalThis?.localStorage?.getItem('SKIP'));
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes} style={{ overflowY: 'hidden' }}>
