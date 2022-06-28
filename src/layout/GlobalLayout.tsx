a2e6e6fb5c623db41cf6ad4c5e0e3d39605bf7ff
Orainsink <ywt1250066597@gmail.com>
2022-06-28 13:05:38 +0800

    Feat: :sparkles: lazy load bg

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -11,12 +11,12 @@ import { useSelector } from 'react-redux';
 import '../assets/css/variables.less';
 import '../assets/css/global.less';
 import BackTop from '../components/BackTop';
-import Bg from '../components/Bg';
 import { useBackTop } from '../hooks';
 import '../assets/css/base.less';
 import { iRootState } from '../redux/store';
 import ErrorBoundary from '../components/ErrorBoundary';
 const Header = lazy(() => import('../components/Header'));
+const Bg = lazy(() => import('../components/Bg'));
 interface Props {
   children: ReactNode;
 }
