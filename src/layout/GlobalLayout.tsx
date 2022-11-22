0115c2143ffbb8bdacfa57ce39ded8829170c32d
Orainsink <ywt1250066597@gmail.com>
2022-11-22 18:15:27 +0800

    Feat: :sparkles: add debugger

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -1,5 +1,6 @@
 import { useEffect, ReactElement, ReactNode, lazy, Suspense } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { useRecoilValue } from 'recoil';
 
 import '../assets/css/variables.css';
 import '../assets/css/global.less';
@@ -8,9 +9,9 @@ import '../assets/css/base.less';
 import { BackTop } from '../components/BackTop';
 import { useBackTop } from '../hooks';
 import { ErrorBoundary } from '../components/ErrorBoundary';
-import { useRecoilValue } from 'recoil';
 import { sceneAtom } from '../store/atom';
 import { defaultTheme } from '../assets/constants/defaultTheme';
+import { DebugObserver } from '../components/Debugger';
 
 const Header = lazy(
   () => import(/* webpackPreload: true */ '../components/Header')
@@ -31,6 +32,7 @@ const GlobalLayout = ({ children }: Props): ReactElement => {
 
   return (
     <ErrorBoundary>
+      {process.env.NODE_ENV === 'development' && <DebugObserver />}
       <ThemeProvider theme={defaultTheme}>
         <div>{children}</div>
         <Suspense fallback={null}>
