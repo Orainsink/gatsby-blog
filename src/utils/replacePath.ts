e2d9fb8e598278391556f6eee011a34d1c11a562
Orainsink <ywt1250066597@gmail.com>
2022-12-14 20:30:16 +0800

    Fix: :bug: replace path bug

--- src/utils/replacePath.ts
+++ src/utils/replacePath.ts
@@ -1,3 +1,5 @@
 // Replacing '/' would result in empty string which is invalid
-export const replacePath = (path: string) =>
-  path === `/` ? path : path.replace(/\/$/, ``);
+export const replacePath = (path: string) => {
+  if (!path) return '';
+  return path === `/` ? path : path.replace(/\/$/, ``);
+};
