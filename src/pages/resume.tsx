377dfa4a3f02f52ebbe0f7d97e975a30828cdda3
Orainsink <ywt1250066597@gmail.com>
2023-02-28 01:19:35 +0800

    Fix: :bug: hide arrow in resume page

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { Seo } from '../components/Seo';
@@ -7,12 +7,20 @@ import {
   AboutSection,
   useElementTween,
 } from '../components/Resume';
+import { hasArrowAtom } from '../store/atom';
+import { useSetRecoilState } from 'recoil';
 
 const ResumeContainer = styled.div`
   background: var(--color-bg-layout);
 `;
 
 const ResumePage = (): ReactElement => {
+  const setHasArrow = useSetRecoilState(hasArrowAtom);
+  useEffect(() => {
+    setHasArrow(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   useElementTween();
   return (
     <ResumeContainer>
