9ad40c64bfc1d094cc1c8b563e75487b10f810ef
Orainsink <ywt1250066597@gmail.com>
2023-03-24 10:38:48 +0800

    Dep: :package: add mdx eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,13 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: [`react-app`],
+  extends: [`react-app`, 'plugin:mdx/recommended'],
   rules: {
     'no-unused-expressions': 'warn',
     strict: 'error',
+  },
+  settings: {
+    'mdx/code-blocks': true,
   },
   root: true,
 };
