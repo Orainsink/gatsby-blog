3a7de95c7a607d0ae099a2df10ae11b571115341
Orainsink <ywt1250066597@gmail.com>
2023-03-31 02:29:14 +0800

    UI: :lipstick: Change font family

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -2,9 +2,6 @@ import Typography from 'typography';
 import { typographyThemeNoriega as noriega } from '../assets/theme/typography';
 import { isDevelopment } from '../../scripts/env';
 
-// @ts-ignore
-delete noriega.googleFonts;
-
 const typography = new Typography(noriega);
 
 // Hot reload typography in development.
